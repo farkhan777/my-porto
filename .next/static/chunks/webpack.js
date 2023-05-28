8437cd785d133058c6fda0a47d0a00f72fd65aa2
Farkhan Hamzah Firdaus <farhanhamzah71@webmail.umm.ac.id>
2023-05-29 01:43:58 +0700

    frist

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f700ac884fc56cfb"; }
+/******/ 		__webpack_require__.h = function() { return "01f246dd34af28ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
