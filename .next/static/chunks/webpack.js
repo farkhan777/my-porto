5aff58031e88fc00663abb6cba173a93356d707f
Farkhan Hamzah Firdaus <farhanhamzah71@webmail.umm.ac.id>
2023-06-18 00:33:48 +0700

    Fix footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d582d2ae0d24d409"; }
+/******/ 		__webpack_require__.h = function() { return "5da26d66217aba2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
