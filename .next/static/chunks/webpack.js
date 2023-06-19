4d045274a72716bccb15a5cabed28770b38ee0e1
Farkhan Hamzah Firdaus <farhanhamzah71@webmail.umm.ac.id>
2023-06-19 13:54:23 +0700

    Update pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08a58476c0ba7408"; }
+/******/ 		__webpack_require__.h = function() { return "25924fe0fc24f61b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
